539743ca957c24f3b952a00c3fcd24a6f3a7414a
Kauan-Code19 <pkauprofissional@gmail.com>
2024-08-30 16:47:29 -0300

    projeto criado.

---
+++ src/main/java/com/kauanProjects/GenArch/GenArchApplication.java
@@ -0,0 +1,13 @@
+package com.kauanProjects.GenArch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenArchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenArchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kauanProjects/GenArch/GenArchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kauanProjects.GenArch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenArchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
